3e0fa0c9970695e52f8a27308eb92056e9cb051c
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2022-01-21 10:46:10 +0100

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AtMoDat_CVs',
+    url='https://github.com/AtMoDat/AtMoDat_CVs',
+    author='Jan Kretzschmar',
+    author_email='jan.kretzschmar@uni-leipzig.de',
+    packages=find_packages(),    
+)
